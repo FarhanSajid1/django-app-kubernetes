15d760988a65da9258a3dcfbed6d944f539537c6
Farhan Sajid <FarhanSajid7861@gmail.com>
2019-04-03 23:18:48 -0400

    k8s deployment

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+# building the images
+docker build -t farhansajid/django-app-k8s:latest -t farhansajid2/django-app-k8s:$SHA -f ./docker/prod/Dockerfile .
+
+
+# pushing the image to docker hub
+docker push farhansajid2/django-app:lastest
+docker push farhansajid2/django-app:$SHA
+
+# applying kubectl configs
+kubectl apply -f k8s
+kubectl set image deployment/app-deployment app=farhansajid2/django-app:$SHA
